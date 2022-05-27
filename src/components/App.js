c0ac16cfb0a6b391484d695b2f74e931377d27a1
Isaiah <ijandicos90@gmail.com>
2022-05-26 21:32:50 -0400

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import About from "./About";
 
 function App() {
   return (
--- src/data/user.js
+++ src/data/user.js
@@ -3,4 +3,7 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
+
+export {username, city, image}
+
 // export all three variables as named exports
